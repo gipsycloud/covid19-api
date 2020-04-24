d6cd44f280e361e445bc2a5e58440697434b3d98
Thant Thet <t@ttkz.me>
2020-04-24 23:30:17 +0630

    Generate city level activity log from raw_data.json

--- src/generate_activity_log.js
+++ src/generate_activity_log.js
@@ -1,104 +1,133 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 const moment = require('moment-timezone');
+const {detailedDiff} = require('deep-object-diff');
 const data = require('../tmp/data.json');
-const data_prev = require('../tmp/data_prev.json');
+const raw_data = require('../tmp/raw_data.json').raw_data;
+const raw_data_prev = require('../tmp/raw_data_prev.json').raw_data;
 
 update_log_file = './tmp/updatelog/log.json';
 var update_log = require("."+update_log_file);
 BOT_TOKEN = process.env.BOT_TOKEN;
 
 
-statewise_new = data.statewise.reduce((arr, row)=>{
-    arr[row.state] = row;
-    return arr;
-},{});
+function makeLocationString(entry) {
+    return `${entry.detectedcity}, ${entry.detectedstate}`
+}
 
-var conf_text;
-var recov_text;
-var death_text;
-var full_text = "";
-var tg_full_text = "";
-var isChanged = false;
+function getChangeSet() {
+    const diff = detailedDiff(raw_data_prev, raw_data);
+    // console.log(util.inspect(diff, false, null, true /* enable colors */));
 
+    const {added, updated} = diff;
 
-data_prev.statewise.forEach(element => {
-    if(element.state=="Total"){
-        return;
-    }
-    isChanged = false;
-    conf_text = null;
-    recov_text = null;
-    death_text = null;
-    text = null;
-    if(parseInt(element.confirmed) < parseInt(statewise_new[element.state].confirmed)){
-        confirmed_diff = statewise_new[element.state].confirmed - element.confirmed;
-        conf_text = confirmed_diff + " new case"+(confirmed_diff==1?"":"s");
-        isChanged=true;
-    }
-    if(parseInt(element.recovered) < parseInt(statewise_new[element.state].recovered)){
-        recovered_diff = statewise_new[element.state].recovered - element.recovered;
-        recov_text = recovered_diff + " recover"+(recovered_diff==1?"y":"ies");
-        isChanged=true;
-    }
-    if(parseInt(element.deaths) < parseInt(statewise_new[element.state].deaths)){
-        death_diff = statewise_new[element.state].deaths - element.deaths;
-        death_text = death_diff + " death"+(death_diff==1?"":"s");
-        isChanged=true;
-    }
-    
-    if(isChanged){
-        text=(conf_text?conf_text+", ":"")+(recov_text?recov_text+", ":"")+(death_text?death_text+", ":"");
-        arr = text.split(", ");
-        if(arr.length>2){
-            arr = text.split(", ");
-            arr = arr.slice(0, -1);
-            arr_last = arr[arr.length-1];
-            arr = arr.slice(0,-1);
-            text = arr.join(", ");
-            text = text + " and "+arr_last
-        }else{
-            arr = arr.slice(0,-1);
-            text = arr.join();
+    const newEntries = Object.values(added);
+
+    let deltaCities = newEntries.map(e => makeLocationString(e)).reduce((prev, location) => {
+        prev[location] = prev[location] || {};
+        prev[location]['case'] = (prev[location]['case'] || 0) + 1;
+        return prev;
+    }, {});
+
+    deltaCities = Object.keys(updated).reduce((prev, index) => {
+
+        const updatedContent = updated[index];
+        const fullContent = raw_data[index];
+        const location = makeLocationString(fullContent);
+
+        if ('currentstatus' in updatedContent && 'dischargeddeceaseddate' in updatedContent) {
+            switch (updatedContent.currentstatus) {
+                case 'Recovered':
+                    prev[location] = prev[location] || {};
+                    prev[location]['recover'] = (prev[location]['recover'] || 0) + 1;
+                    break;
+                case 'Deceased':
+                    prev[location] = prev[location] || {};
+                    prev[location]['death'] = (prev[location]['death'] || 0) + 1;
+                    break;
+            }
         }
-        text = text +" in "+element.state
-        full_text = full_text + text + "\n"
-        
-        total = statewise_new["Total"];
+
+        return prev;
+    }, deltaCities);
+
+    return deltaCities;
+}
+
+(function generateLog() {
+    const changeset = getChangeSet();
+
+    var full_text = "";
+    var tg_full_text = "";
+
+    statewise = data.statewise.reduce((arr, row)=>{
+        arr[row.state] = row;
+        return arr;
+    },{});
+
+    const isChanged = Object.keys(changeset).length !== 0;
+    if (isChanged) {
+        total = statewise["Total"];
+
+        const texts = Object.keys(changeset).reduce((prev, location) => {
+            const change = changeset[location];
+            prev[location] = Object.keys(change).reduce((prev, type) => {
+                const count = change[type];
+                const newOrSpace = type === 'case' ? ' new ' : ' ';
+                const sORes = type === 'recover' ? 'ies' : 's';
+                const pluralMaybe = count === 1 ? '' : sORes;
+                prev.push(`${count}${newOrSpace}${type}${pluralMaybe}`);
+                return prev;
+            }, []);
+            return prev
+        }, {});
+
+        full_text = Object.keys(texts).reduce((prev, location) => {
+            const parts = texts[location];
+            let text = '';
+            if (parts.length > 2) {
+                const last = parts.pop();
+                text = `${parts.join(', ')} and ${last}`;
+            } else {
+                text = parts.join(', ');
+            }
+            prev = prev === '' ? '' : `${prev}\n`;
+            return `${prev}${text} in ${location}`;
+        }, "")
+
         tg_full_text = full_text + "\n"
             + "``` Total cases: (↑" + total.deltaconfirmed + ") " + total.confirmed
             + "\n" + " Recovered  : (↑" + total.deltarecovered + ") " + total.recovered
             + "\n" + " Deaths     : (↑" + total.deltadeaths + ") " + total.deaths + "```";
-    }   
-});
+    }
 
-if (full_text!=""){
-    console.log(full_text);
-    
-    const now = moment().unix()
-    entry = {};
-    entry.update = full_text;
-    entry.timestamp = now;
-    update_log.push(entry);
-    update_log = update_log.slice(-50)
-    
-    fs.writeFileSync(update_log_file, JSON.stringify(update_log, null, 2));
+    if (full_text!=""){
+        console.log(full_text);
+        
+        const now = moment().unix()
+        entry = {};
+        entry.update = full_text;
+        entry.timestamp = now;
+        update_log.push(entry);
+        update_log = update_log.slice(-50)
+        
+        fs.writeFileSync(update_log_file, JSON.stringify(update_log, null, 2));
 
-    var date = moment.unix(now);
-    formated_time = date.tz("Asia/Rangoon").format("MMMM DD, hh:mm A");
-    console.log(formated_time)
-    
+        var date = moment.unix(now);
+        formated_time = date.tz("Asia/Rangoon").format("MMMM DD, hh:mm A");
+        console.log(formated_time)
+        
 
-    url = encodeURI("https://api.telegram.org/bot"+BOT_TOKEN+"/sendmessage?parse_mode=Markdown&chat_id=-258782427&text=_"
-        +formated_time+"_\n\n"
-        +tg_full_text
-        +"\n\n*covid19.ttkz.me*");
-    // console.log(url);
-    let settings = { method: "Get" };
-    fetch(url, settings).then(res => res.json())
-	.then(json => console.log(json));
-    
-}else{
-    console.log("No updates this time!");
-}
-
+        url = encodeURI("https://api.telegram.org/bot"+BOT_TOKEN+"/sendmessage?parse_mode=Markdown&chat_id=-258782427&text=_"
+            +formated_time+"_\n\n"
+            +tg_full_text
+            +"\n\n*covid19.ttkz.me*");
+        // console.log(url);
+        let settings = { method: "Get" };
+        fetch(url, settings).then(res => res.json())
+        .then(json => console.log(json));
+        
+    }else{
+        console.log("No updates this time!");
+    }
+})();
